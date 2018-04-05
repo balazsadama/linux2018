b9340e835e720b402433311dd1ef5c426a2b5729
balazsadama <balazs.adam.a@gmail.com>
2018-04-05 21:01:22 +0300

    Add files via upload

---
+++ L04_01_baam0146.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# nev: Balazs Adam-Attila
+# felhasznalo: baam0146
+# feladat: Írassa ki a paraméterként megadott katalógusban és alaktalógusaiban
+# szereplő szöveges file-ok "."-al vagy számjeggyel kezdődő sorait.
+
+if [ $# -ne 1 ]
+then
+        echo 'Parameter: directory'
+        exit 1
+fi
+
+if [ -d $1 ]
+then
+	find $1 -type f
+	files=($(find $1 -type f))
+	for f in $(find $1 -type f) do
+		#echo $f
+	done
+
+#	echo $1
+#	for d in ~/labor4_grep/level1/*; do
+#		if [ -f "$d" ] then
+#			echo "$d"
+#		fi
+#	done
+else
+	echo 'Invalid directory given.'
+fi
